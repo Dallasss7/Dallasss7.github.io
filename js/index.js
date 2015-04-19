939e3f38dcfa2f3d5996470f3acf9ff23b11291f
Dallasss7 <mr.dalsummers@gmail.com>
2015-04-18 23:59:49 -0700

    change of portfolio

---
+++ js/index.js
@@ -0,0 +1,132 @@
+var winWidth = $(window).width();
+
+
+
+
+function parallax(scrollY, heightFooter){
+	
+	if(scrollY >= heightFooter){
+		$('footer').css({'bottom': '0px'});
+	}
+
+	else{
+		$('footer').css({'bottom': '-' + heightFooter + 'px'});
+	}
+
+}
+
+
+
+$(window).load(function(){
+	var windowHeight = $(window).height() -60,
+		footerHeight = $('footer').height(),
+		heightDocument =  $(window).height() + $('.content').height() + $('footer').height() -60;
+
+	console.log(heightDocument);
+
+	$('#main, #wrapper').css({ 'height': heightDocument + 'px'});
+
+	$('header').css({ 'height': windowHeight + 'px', 'line-height' : windowHeight + 'px'});
+
+	$('.parallax').css({ 'margin-top': windowHeight + 'px'});
+
+	parallax(window.scrollY, footerHeight);
+
+	window.onscroll = function(){
+		
+		var scroll = window.scrollY;
+
+		$('#main').css({ 'top': '-' + scroll +'px'});
+
+		$('header').css({ 'background-position-y' : 50 - (scroll * 100 / heightDocument) + '%'})
+	
+		parallax(scroll, footerHeight);
+	}
+
+});
+
+function jFade(event){
+
+$('#dallas-header').fadeOut();
+}
+
+function jFadeIn(event){
+
+$('#dallas-header').fadeIn();
+}
+
+var controller = new ScrollMagic.Controller();
+
+
+var scene = new ScrollMagic.Scene({
+	duration: 700,    
+	triggerElement: "#gallery",
+	reverse:true
+    })
+
+var navscene = new ScrollMagic.Scene({
+	duration: 3000,    
+	offset: 722,
+	reverse:true
+    })
+
+
+scene.update();
+scene.on("enter", jFade);
+scene.on("leave", jFadeIn);
+scene.addTo(controller); 
+
+
+navscene.update();
+navscene.setClassToggle(".nav", "sticky-nav");
+navscene.addTo(controller); 
+
+		$('#con').hover(function () {
+	      $('.nav-port').css({'visibility':'hidden'});
+	      $('.contact-info').css({'display':'inline-block'});
+	    }, function () {
+	      $('.nav-port').css({'visibility': 'visible'});
+	      $('.contact-info').css({'display':'none'});
+	    });
+	
+
+
+$('#portfolio').on('click', function (){
+	window.location.href = "file:///Users/DSummers/Desktop/programs/sublime/new-port/index.html#portfolio";
+})
+
+
+
+
+
+
+console.log(winWidth, "WINDOW WIDTH")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
