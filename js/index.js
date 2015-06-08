ba1d8e917f472cfd7f563f9353e3fba524d64eb0
Dallasss7 <mr.dalsummers@gmail.com>
2015-06-07 20:00:53 -0700

    tweeking more with broken padding on about

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ function parallax(scrollY, heightFooter){
 $(window).load(function(){
 	var windowHeight = $(window).height(),
 		footerHeight = $('footer').height(),
-		heightDocument =  $(window).height() + $('.content').height() + $('footer').height();
+		heightDocument =  $(window).height() + $('.content').height();
 
 	console.log(heightDocument);
 
