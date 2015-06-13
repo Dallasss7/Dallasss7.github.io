df690dc8ec3b81d91080cbd372544755212c25f7
Dallasss7 <mr.dalsummers@gmail.com>
2015-06-12 17:26:22 -0700

    div size edit

--- js/index.js
+++ js/index.js
@@ -74,41 +74,10 @@ var scene = new ScrollMagic.Scene({
 	reverse:true
     })
 
-var navscene = new ScrollMagic.Scene({
-	duration: 3000,    
-	offset: 722,
-	reverse:true
-    })
-
-
 scene.update();
 scene.on("enter", jFade);
 scene.on("leave", jFadeIn);
 scene.addTo(controller); 
-
-
-navscene.update();
-navscene.setClassToggle(".nav", "sticky-nav");
-navscene.addTo(controller); 
-
-		$('#con').hover(function () {
-	      $('.nav-port').css({'visibility':'hidden'});
-	      $('.contact-info').css({'display':'inline-block'});
-	    }, function () {
-	      $('.nav-port').css({'visibility': 'visible'});
-	      $('.contact-info').css({'display':'none'});
-	    });
-	
-
-
-$('#portfolio').on('click', function (){
-	window.location.href = "file:///Users/DSummers/Desktop/programs/sublime/new-port/index.html#portfolio";
-})
-
-
-
-
-
 
 console.log(winWidth, "WINDOW WIDTH")
 
