25c16900d0612bd5c84879ec8d3c08c1702aafe3
Dallasss7 <mr.dalsummers@gmail.com>
2019-03-25 19:34:03 -0700

    added skills section and changed background

--- js/index.js
+++ js/index.js
@@ -16,6 +16,17 @@
     {
       $(".skills_body").fadeIn();
       $(".skills").addClass("active");
+
+      $('.chart').easyPieChart({
+        scaleColor: false,
+        lineWidth: 10,
+        lineCap: 'butt',
+        barColor: '#73b14b',
+        trackColor: '#C1C1C1' ,
+        size: 90,
+        animate: 800
+      });
     }
   });
+
 })();
